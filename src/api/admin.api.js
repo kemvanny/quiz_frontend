d5aa55a29e2ed49cbcdf8d3819978022f6f2419d
Vit Socheata <145731923+VitSocheata@users.noreply.github.com>
2026-05-16 23:32:46 +0700

    fetch usermanagement

--- src/api/admin.api.js
+++ src/api/admin.api.js
@@ -1,6 +1,7 @@
 //សម្រាប់ Manage Users, View All Quizzes
 import api from './axiosInstance';
 
-export const getAllUsers = () => api.get('/admin/users');
+export const getAllUsers = () => api.get('/admin/getAllUsers');
 export const deleteUser = (id) => api.delete(`/admin/users/${id}`);
 export const updateStatus = (id, data) => api.put(`/admin/users/status/${id}`, data);
+export const getSummerise = () => api.get('/admin/dashboard/overview');
