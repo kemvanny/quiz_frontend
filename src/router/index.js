53df55e74a7bed5c80673214dba702ffa53853d4
Vit Socheata <145731923+VitSocheata@users.noreply.github.com>
2026-06-23 09:20:15 +0700

    change font change style in dashboard delete system health

--- src/router/index.js
+++ src/router/index.js
@@ -101,12 +101,6 @@ const router = createRouter({
           meta: { title: "Profile Admin" }
         },
         {
-          path: 'system-health',
-          name: 'SystemHealth',
-          component: SystemHealth,
-          meta: { title: "System Health" }
-        },
-        {
           path: 'room-managements',
           name: 'RoomManagements',
           component: RoomManagements,
